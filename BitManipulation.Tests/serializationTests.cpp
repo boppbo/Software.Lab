0543585e7f2107873f79941256f7473ea494bbb2
Boris Bopp <boris.bopp@gmail.com>
2016-03-11 20:21:02 +0100

    Add tests for bit manipulation

---
+++ BitManipulation.Tests/serializationTests.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "../BitManipulation/serialization.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BitmanipulationTests
+{
+	TEST_CLASS(SerializationTests)
+	{
+	public:
+
+		TEST_METHOD(serializationAndDeserializationShouldReturnOriginalValue)
+		{
+			Status state = Finish, stateTwo;
+			Numbers no = Fifteen, noTwo;
+
+			short buf;
+			serialize(state, no, &buf);
+			deserialize(buf, &stateTwo, &noTwo);
+
+			Assert::IsTrue(state == stateTwo);
+			Assert::IsTrue(no == noTwo);
+		}
+	};
+}
---
+++ BitManipulation.Tests/switchLowHighByteTests.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "../BitManipulation/switchLowHighByte.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BitmanipulationTests
+{
+	TEST_CLASS(SwitchLowHighByteTests)
+	{
+	public:
+
+		TEST_METHOD(switchLowHighByteShouldReturnPositiveValue)
+		{
+			Assert::IsTrue(0x3412 == switchLowHighByte(0x1234));
+		}
+
+		TEST_METHOD(switchLowHighByteShouldReturnInputForSecondCall)
+		{
+			Assert::IsTrue(0x1234 == switchLowHighByte(switchLowHighByte(0x1234)));
+		}
+
+	};
+}
