f2a84281722698f213569fc0264ecabf027db526
Boris Bopp <boris.bopp@gmail.com>
2016-11-15 21:46:10 +0100

    Update switchLowHighByte.c
    Fix for negative values

--- BitManipulation/switchLowHighByte.c
+++ BitManipulation/switchLowHighByte.c
@@ -10,8 +10,8 @@
 /// <returns>	The new integer. </returns>
 BITMANIPULATION_API short int switchLowHighByte(short int i)
 {
-	short int low = i & 0x00FF;
-	short int high = i & 0xFF00;
+	auto low = (unsigned short int) i & 0x00FF;
+	auto high = (unsigned short int) i & 0xFF00;
 
 	return (low << 8) | (high >> 8);
 }
